887941119002e523cd465def2bbd264ee5ad4346
yezzi <yeye@yeyedeMac.local>
2016-04-12 18:26:12 +0800

    h
    h

--- TableViewFun/ViewController.swift
+++ TableViewFun/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data = ["😄 Human", "🐱 Necko", "🐶 Doggy", "🎄 Tree", "🎁 Gift"]
+    
+    var normalText: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +29,38 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("NormalCell", forIndexPath: indexPath)
+        
+        let text = data[indexPath.row]
+        cell.textLabel?.text = text
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        normalText = cell?.textLabel?.text
+        performSegueWithIdentifier("showNormalViewController", sender: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showNormalViewController" {
+            let viewController = segue.destinationViewController as! NormalViewController
+            viewController.view.hidden = false
+            viewController.normalLabel.text = normalText
+        }
+    }
 }
 
